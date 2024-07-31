14919133d3c070c5477222385bfc1aaaf8a1c3df
think <lingwh.walkman@hotmail.com>
2024-07-31 09:24:05 +0800

    更新依赖

--- java-enterprise-edition-springboot2/chapter-03-003-springboot-senior-quartz/src/main/java/com/dragonsoft/Application.java
+++ java-enterprise-edition-springboot2/chapter-03-003-springboot-senior-quartz/src/main/java/com/dragonsoft/Application.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class Application {
+    //https://blog.csdn.net/qq_45087487/article/details/132063403
     //https://www.w3cschool.cn/quartz_doc/quartz_doc-h4ux2cq6.html
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
