c745f10f1128084cc116fc77ed42fdd568a5a9be
think <lingwh.walkman@hotmail.com>
2024-07-31 17:59:09 +0800

    springboot读取配置文件

---
+++ java-enterprise-edition-springboot2/chapter-01-008-springboot-basic-support-read-configuration-file/src/main/java/com/dragonsoft/configurationProperties_bind_with_out_bean/LibraryBindWithoutBean.java
@@ -0,0 +1,64 @@
+package com.dragonsoft.configurationProperties_bind_with_out_bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@ConfigurationProperties(prefix = "library")
+public class LibraryBindWithoutBean {
+    private String location;
+    private List<Book> books;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    static class Book {
+        private String name;
+        private String description;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return "Book{" +
+                    "name='" + name + '\'' +
+                    ", description='" + description + '\'' +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "location='" + location + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
