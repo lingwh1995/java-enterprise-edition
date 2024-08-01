3a613c8bf1a3f0cfb7b2ece4566aa4a2939abf73
think <lingwh.walkman@hotmail.com>
2024-08-01 09:55:39 +0800

    springboot解析yml

---
+++ java-enterprise-edition-springboot2/chapter-01-009-springboot-read-yml-configuration-file/src/main/java/com/dragonsoft/Application.java
@@ -0,0 +1,16 @@
+package com.dragonsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ronin
+ * @version V1.0
+ * @since 2020/3/5 13:49
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
