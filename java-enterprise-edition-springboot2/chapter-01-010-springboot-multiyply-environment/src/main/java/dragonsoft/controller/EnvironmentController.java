bf879669816b0c3a6f4e7b26a93461ced953e94b
think <lingwh.walkman@hotmail.com>
2024-09-05 18:33:07 +0800

    spring区分多环境

---
+++ java-enterprise-edition-springboot2/chapter-01-010-springboot-multiyply-environment/src/main/java/dragonsoft/controller/EnvironmentController.java
@@ -0,0 +1,23 @@
+package dragonsoft.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class EnvironmentController {
+
+    @Value("${spring.profiles.active}")
+    private String environment;
+
+    /**
+     *  访问: http://localhost:8200/environment
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/environment")
+    public String getCurrentEnvironment() {
+        return "当前环境:" + environment;
+    }
+}
