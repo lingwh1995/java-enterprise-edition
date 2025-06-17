e5b4faa17e79e386a12b0368aa5c21d2203f86dc
think <lingwh.walkman@hotmail.com>
2025-06-17 18:52:31 +0800

    修改项目结构

--- java-enterprise-edition-06-netty/chapter-01-01-jdk-nio/src/main/java/org/bluebridge/chapter_01/_09_FileChannelTransferToTest.java
+++ java-enterprise-edition-06-netty/chapter-01-01-jdk-nio/src/main/java/org/bluebridge/chapter_01/_09_FileChannelTransferToTest.java
@@ -12,16 +12,20 @@ import java.nio.channels.FileChannel;
 @Slf4j
 public class _09_FileChannelTransferToTest {
 
+    /**
+     * 传输小于2G的数据
+     */
     @Test
-    public void FileChannelTransferToTest() {
-        try(FileChannel from = new FileInputStream("files/from.txt").getChannel();
-            FileChannel to = new FileOutputStream("files/to.txt").getChannel()){
+    public void FileChannelTransferToLessThan2GTest() {
+        try (FileChannel from = new FileInputStream("files/from.txt").getChannel();
+            FileChannel to = new FileOutputStream("files/to.txt").getChannel()
+        ) {
             // 获取文件大小
             long size = from.size();
             log.debug("文件大小：{}", size);
             // 效率高，会使用系统底层零拷贝进行优化，一次最多可以传输2G文件
             long currentTransferDataLength = from.transferTo(0, from.size(), to);
-            System.out.println("本次传输的数据长度 = " + currentTransferDataLength + "byte");
+            System.out.println("本次传输的数据长度 = " + currentTransferDataLength + " byte");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -29,4 +33,32 @@ public class _09_FileChannelTransferToTest {
         }
     }
 
+    /**
+     * 传输大于2G的数据
+     */
+    @Test
+    public void FileChannelTransferToLessThan2GTest1() {
+        // 返回当前JVM运行的纳秒级时间戳
+        long start = System.nanoTime();
+        try (FileChannel from = new FileInputStream("E:\\nio\\from.iso").getChannel();
+             FileChannel to = new FileOutputStream("E:\\nio\\to.iso").getChannel()
+        ) {
+            // 获取文件大小
+            long size = from.size();
+            log.debug("文件大小：{}", size);
+            long copySize = 0;
+            while(copySize < size){
+                copySize += from.transferTo(copySize, size, to);
+                log.debug("已经拷贝传输的数据大小：{}", copySize);
+                System.out.println("to.position() = " + to.position());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // 返回当前JVM运行的纳秒级时间戳
+        long end = System.nanoTime();
+        System.out.println("transferTo 用时：" + (end - start) / 1000_000_000.0 + "秒");
+    }
 }
---
+++ java-enterprise-edition-06-netty/chapter-01-01-jdk-nio/src/main/java/org/bluebridge/chapter_01/_10_JDK7PathAndPathsTest.java
@@ -0,0 +1,44 @@
+package org.bluebridge.chapter_01;
+
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * JDK7 新增了 Path 和 Paths 类
+ *  Path 用来表示文件路径
+ *  Paths 是工具类，用来获取 Path 实例
+ */
+public class _10_JDK7PathAndPathsTest {
+
+    /**
+     * 测试JDK7 新增的 Path 和 Paths 类
+     */
+    @Test
+    public void testJDK7PathAndPaths() {
+        // 相对路径
+        Path path = Paths.get("1.txt");
+        System.out.println(path.toAbsolutePath());
+
+        // 绝对路径1
+        path = Paths.get("d:\\1.txt");
+        System.out.println(path.toAbsolutePath());
+
+        // 绝对路径2
+        path = Paths.get("d:/1.txt");
+        System.out.println(path.toAbsolutePath());
+
+        // 第二个参数可以是文件名
+        path = Paths.get("d:/a", "1.txt");
+        System.out.println(path.toAbsolutePath());
+        // 第二个参数也可以是文件夹
+        path = Paths.get("d:/a", "b");
+        System.out.println(path.toAbsolutePath());
+
+        // 正常化路径
+        path = Paths.get("d:\\data\\projects\\a\\..\\b");
+        System.out.println(path);
+        System.out.println(path.normalize());
+    }
+}
