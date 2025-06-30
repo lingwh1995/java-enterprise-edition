2b6228152e01487dd8100c927c5f9c8700030bde
think <lingwh.walkman@hotmail.com>
2025-06-30 16:50:23 +0800

    修改项目结构

--- java-enterprise-edition-03-springboot2/chapter-02-08-tools-springutils/src/main/java/org/bluebridge/StopWatchTest.java
+++ java-enterprise-edition-03-springboot2/chapter-02-08-tools-springutils/src/main/java/org/bluebridge/StopWatchTest.java
@@ -4,31 +4,57 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.util.StopWatch;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class StopWatchTest {
 
     /**
-     * 测试StopWatch
+     * 测试StopWatch HelloWorld 案例
      */
     @Test
-    public void testStopWatch() {
+    public void testStopWatchHelloWorld() throws InterruptedException {
         // 创建一个StopWatch对象
-        StopWatch stopWatch = new StopWatch();
+        StopWatch stopWatch = new StopWatch("秒表");
         // 开始计时
-        stopWatch.start();
-
-        task();
-
+        stopWatch.start("task-1");
+        task1();
         // 停止计时
         stopWatch.stop();
-        System.out.println(stopWatch.getLastTaskName() + "执行时间：" + stopWatch.getLastTaskTimeMillis());
+        System.out.println("StopWatch名称：" + stopWatch.getLastTaskName() + "，执行时间：" + stopWatch.getLastTaskTimeMillis());
+    }
+    /**
+     * 测试StopWatch HelloWorld 案例
+     */
+    @Test
+    public void testStopWatch() throws InterruptedException {
+        // 创建一个StopWatch对象
+        StopWatch stopWatch = new StopWatch("秒表");
+        // task-1开始计时
+        stopWatch.start("task-1");
+        task1();
+        // task-1停止计时
+        stopWatch.stop();
+
+        // task-2开始计时
+        stopWatch.start("task-2");
+        task2();
+        // task-2停止计时
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
     }
 
-    private void task() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    /**
+     * 任务一：睡眠1000毫秒
+     */
+    private void task1() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(1000);
+    }
+
+    /**
+     * 任务二：睡眠2000毫秒
+     */
+    private void task2() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(2000);
     }
 }
