13ffeb92a8cac4307f2442295bf3cf65c871743f
think <lingwh.walkman@hotmail.com>
2025-07-16 18:43:23 +0800

    修改项目结构

---
+++ java-enterprise-edition-06-netty/chapter-01-03-jdk-aio/src/main/java/org/bluebridge/chapter_01_aio/_01_FileChannelTest.java
@@ -0,0 +1,47 @@
+package org.bluebridge.chapter_01_aio;
+
+import lombok.extern.slf4j.Slf4j;
+import org.bluebridge.ByteBufferUtil;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @author lingwh
+ * @desc
+ * @date 2025/7/16 18:34
+ */
+@Slf4j
+public class _01_FileChannelTest {
+
+    public static void main(String[] args) throws IOException {
+        try{
+            AsynchronousFileChannel channel = AsynchronousFileChannel
+                    .open(Paths.get("java-enterprise-edition-06-netty/chapter-01-03-jdk-aio/files/aio.txt"), StandardOpenOption.READ);
+            ByteBuffer buffer = ByteBuffer.allocate(2);
+            log.debug("begin......");
+            channel.read(buffer, 0, null, new CompletionHandler<Integer, ByteBuffer>() {
+                @Override
+                public void completed(Integer result, ByteBuffer attachment) {
+                    log.debug("read completed......{}", result);
+                    buffer.flip();
+                    ByteBufferUtil.debugAll(buffer);
+                }
+
+                @Override
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    log.debug("read failed......");
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.debug("do other things......");
+        System.in.read();
+    }
+
+}
