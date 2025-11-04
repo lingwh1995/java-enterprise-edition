3977b736bcc9f001ba75b4bb9496c2ae9040eacf
think <lingwh.work@hotmail.com>
2025-11-04 17:00:19 +0800

    修改项目结构

---
+++ java-enterprise-edition-03-springboot2/chapter-02-16-integration-protobuf/src/test/java/org/bluebridge/ProtobufControllerTest.java
@@ -0,0 +1,74 @@
+package org.bluebridge;
+
+import lombok.extern.slf4j.Slf4j;
+import org.bluebridge.domain.PersonProto;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author lingwh
+ * @desc protobuf 测试类
+ * @date 2025/11/4 16:22
+ */
+@Slf4j
+@SpringBootTest(classes = Application.class)
+public class ProtobufControllerTest {
+
+//    @Resource
+//    private RestTemplate restTemplate;
+    private RestTemplate restTemplate = new RestTemplate(Collections.singletonList(new ProtobufHttpMessageConverter()));
+    // 测试 GET 接口（响应 Protobuf）
+    @Test
+    public void testGetPersonById() {
+        String url = "http://localhost:8080/protobuf/1";
+        HttpHeaders headers = new HttpHeaders();
+        // 声明接收 Protobuf 格式响应
+        headers.setAccept(MediaType.parseMediaTypes("application/x-protobuf"));
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        // 发送请求，指定响应类型为 PersonProto.Person
+        ResponseEntity<PersonProto.Person> response = restTemplate.exchange(
+                url, HttpMethod.GET, requestEntity, PersonProto.Person.class
+        );
+
+        // 验证结果
+        assertEquals(200, response.getStatusCodeValue());
+        PersonProto.Person person = response.getBody();
+        log.info("person: {}", person);
+    }
+
+    // 测试 POST 接口（接收 Protobuf 请求，响应 Protobuf）
+    @Test
+    public void testQueryPerson() {
+        String url = "http://localhost:8080/protobuf/query";
+        HttpHeaders headers = new HttpHeaders();
+        // 声明请求格式为 Protobuf
+        headers.setContentType(MediaType.valueOf("application/x-protobuf"));
+        // 声明响应格式为 Protobuf
+        headers.setAccept(MediaType.parseMediaTypes("application/x-protobuf"));
+
+        // 构建 Protobuf 请求参数
+        PersonProto.Person request = PersonProto.Person.newBuilder().setId(2).build();
+        HttpEntity<PersonProto.Person> requestEntity = new HttpEntity<>(request, headers);
+
+        // 发送请求，指定响应类型为 PersonProto.Person
+        ResponseEntity<PersonProto.Person> response = restTemplate.exchange(
+                url, HttpMethod.POST, requestEntity, PersonProto.Person.class
+        );
+
+        // 验证结果
+        assertEquals(200, response.getStatusCodeValue());
+        PersonProto.Person person = response.getBody();
+        log.info("person: {}", person);
+    }
+
+}
