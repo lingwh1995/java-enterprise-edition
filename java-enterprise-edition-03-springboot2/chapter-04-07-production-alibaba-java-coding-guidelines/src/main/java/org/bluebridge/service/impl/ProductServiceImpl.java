81e92ef91ab2f09aae95a84cabc43f27beeb5fe0
lingwh <lingwh.walkman@homail.com>
2025-12-17 01:25:37 +0800

    修改项目结构

--- java-enterprise-edition-03-springboot2/chapter-04-07-production-alibaba-java-coding-guidelines/src/main/java/org/bluebridge/service/impl/ProductServiceImpl.java
+++ java-enterprise-edition-03-springboot2/chapter-04-07-production-alibaba-java-coding-guidelines/src/main/java/org/bluebridge/service/impl/ProductServiceImpl.java
@@ -8,7 +8,7 @@ import org.bluebridge.dto.ProductUpdateDTO;
 import org.bluebridge.dto.ProductPatchDTO;
 import org.bluebridge.dto.ProductQueryDTO;
 import org.bluebridge.common.exception.ProductException;
-import org.bluebridge.convertor.ProductConvertor;
+import org.bluebridge.common.convertor.ProductConvertor;
 import org.bluebridge.dao.mapper.ProductMapper;
 import org.bluebridge.service.ProductService;
 import org.bluebridge.controller.vo.ProductVO;
