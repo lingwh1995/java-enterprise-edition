fbe507edced4aca793038596d7ec5b78be9d9c05
lingwh <lingwh.walkman@homail.com>
2025-12-24 22:17:35 +0800

    修改项目结构

--- java-enterprise-edition-03-springboot2/chapter-04-07-production-alibaba-java-coding-guidelines/src/main/resources/db/init.sql
+++ java-enterprise-edition-03-springboot2/chapter-04-07-production-alibaba-java-coding-guidelines/src/main/resources/db/init.sql
@@ -2,7 +2,7 @@ use javaee;
 
 -- 创建商品表
 CREATE TABLE IF NOT EXISTS `t_product` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+    `id` bigint(20) PRIMARY KEY AUTO_INCREMENT COMMENT '商品ID',
     `name` varchar(100) NOT NULL COMMENT '商品名称',
     `description` varchar(500) DEFAULT NULL COMMENT '商品描述',
     `price` decimal(10,2) NOT NULL COMMENT '商品价格',
@@ -11,9 +11,7 @@ CREATE TABLE IF NOT EXISTS `t_product` (
     `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `is_deleted` int(11) NOT NULL DEFAULT '0' COMMENT '删除标记（0：未删除，1：已删除）',
-    PRIMARY KEY (`id`),
     KEY `idx_name` (`name`),
-    KEY `idx_status` (`status`),
     KEY `idx_is_deleted` (`is_deleted`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品表';
 
