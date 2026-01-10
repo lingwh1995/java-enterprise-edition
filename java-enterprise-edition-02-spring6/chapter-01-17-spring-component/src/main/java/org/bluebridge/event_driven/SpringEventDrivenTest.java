76087d60c6db1541ddd1f951432aa4f5123f7267
lingwh <lingwh.walkman@homail.com>
2026-01-10 15:26:26 +0800

    修改项目结构

--- java-enterprise-edition-02-spring6/chapter-01-17-spring-component/src/main/java/org/bluebridge/event_driven/SpringEventDrivenTest.java
+++ java-enterprise-edition-02-spring6/chapter-01-17-spring-component/src/main/java/org/bluebridge/event_driven/SpringEventDrivenTest.java
@@ -1,6 +1,5 @@
 package org.bluebridge.event_driven;
 
-import org.bluebridge.aware.SpringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
--- java-enterprise-edition-02-spring6/chapter-01-17-spring-component/src/main/java/org/bluebridge/event_driven/UserService.java
+++ java-enterprise-edition-02-spring6/chapter-01-17-spring-component/src/main/java/org/bluebridge/event_driven/UserService.java
@@ -1,5 +1,7 @@
 package org.bluebridge.event_driven;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bluebridge.aware.SpringUtils;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private static final Logger log = LogManager.getLogger(MessageListener.class);
+
     public void register(String name) {
         // 打印注册成功日志
-        System.out.println("用户注册成功：" + name);
+        log.info("用户注册成功：{}", name);
         // 使用 SpringUtils 发布事件，或者注入 ApplicationEventPublisher
         SpringUtils.publishEvent(new UserRegisterEvent(this, name));
     }
