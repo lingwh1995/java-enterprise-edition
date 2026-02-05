ae3097bd752800b4cae421b732aeae0d59864d8c
think <lingwh.work@hotmail.com>
2026-02-05 18:26:19 +0800

    修改项目结构

--- java-enterprise-edition-03-springboot2/chapter-04-08-production-alibaba-java-coding-guidelines-product-2/src/main/java/org/bluebridge/guard/constant/Pbkdf2Constants.java
+++ java-enterprise-edition-03-springboot2/chapter-04-08-production-alibaba-java-coding-guidelines-product-2/src/main/java/org/bluebridge/guard/constant/Pbkdf2Constants.java
@@ -10,16 +10,19 @@ public final class Pbkdf2Constants {
     /**
      * PBKDF2 加密算法
      */
-    public static String ALGORITHM;
+    public static String ALGORITHM = "PBKDF2WithHmacSHA1";
 
     /**
      * PBKDF2 密钥长度（位）
      */
-    public static int KEY_SIZE_BITS;
+    public static int KEY_SIZE_BITS = 256;
 
     /**
      * PBKDF2 迭代次数
+     *  最低推荐值：10,000 次迭代（适用于一般应用）
+     *  增强安全性：100,000 次迭代（适用于对安全性要求较高的场景）
+     *  极高安全性：500,000 次及以上（适用于金融、军事等高敏感领域）
      */
-    public static int ITERATION_COUNT;
+    public static int ITERATION_COUNT = 10000;
 
 }
